1b77bc4c586c0f6baaf9e17ca13f78fb370c8502
Yuchi Yang <matthew.y0617@gmail.com>
2018-07-05 20:04:17 +0200

    v0

--- main.py
+++ main.py
@@ -3,77 +3,73 @@
 # @Time    : 2018/5/23 下午1:19
 # @Author  : Yang Yuchi
 
+import argparse
 import threading
-import numpy as np
-import multiprocessing
+import multiprocessing as mp
 import tensorflow as tf
 import gym
-import matplotlib.pyplot as plt
-from network import ActorCriticNet, Worker, ep_rewards
+from network import ActorCriticNet, Agent
 
 
 # Hyper parameters
-GAME = 'CartPole-v0'
-GLOBAL_NET_SCOPE = 'global_net'
-N_WORKERS = multiprocessing.cpu_count()
-RANDOM_SEED = 1
-BATCH_SIZE = 4
-RENDER = True
+parser = argparse.ArgumentParser(description='Actor Critic with Experience Replay')
+parser.add_argument('--seed', type=int, default=11, help='Random seed')
+parser.add_argument('--num-agents', type=int, default=mp.cpu_count(), metavar='N', help='Number of training agents')
+parser.add_argument('--max-iterations', type=int, default=10000, metavar='STEPS', help='Maximum training steps')
+parser.add_argument('--t-max', type=int, default=100, metavar='STEPS',
+                    help='Max number of forward steps for on-policy learning before update')
+parser.add_argument('--max-episode-length', type=int, default=500, metavar='LENGTH', help='Maximum episode length')
+parser.add_argument('--replay-start', type=int, default=20000, metavar='EPISODES',
+                    help='Number of transitions to save before starting off-policy training')
+parser.add_argument('--batch-size', type=int, default=16, metavar='SIZE', help='Off-policy batch size')
+parser.add_argument('--num-hidden', type=int, default=100, metavar='SIZE', help='Number of hidden neurons')
+parser.add_argument('--memory-capacity', type=int, default=10000, metavar='CAPACITY',
+                    help='Experience replay memory capacity')
+parser.add_argument('--max-stored-episode-length', type=int, default=10000, metavar='LENGTH',
+                    help='Maximum length of a stored episode')
+parser.add_argument('--replay-ratio', type=int, default=4, metavar='r', help='Ratio of off-policy to on-policy updates')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='γ', help='RL discount factor')
+parser.add_argument('-c', type=float, default=10, metavar='c', help='Importance weight truncation (max) value')
+parser.add_argument('--trust-region', action='store_true', help='Use trust region')
+parser.add_argument('--alpha', type=float, default=0.99, metavar='α', help='Average policy decay rate')
+parser.add_argument('--delta', type=float, default=1, metavar='δ', help='Trust region threshold value')
+parser.add_argument('--learning-rate', type=float, default=0.0007, metavar='η', help='Learning rate')
+parser.add_argument('--env', type=str, default='CartPole-v1', help='environment name')
+parser.add_argument('--output-graph', action='store_true', help='Output Tensor board graph')
+parser.add_argument('--max-gradient-norm', type=float, default=40, metavar='VALUE', help='Gradient L2 normalisation')
 
-tf.set_random_seed(RANDOM_SEED)
+args = parser.parse_args()
 
-env = gym.make(GAME).unwrapped
-env.seed(RANDOM_SEED)
+gym.logger.set_level(gym.logger.ERROR)  # Disable Gym warnings
 
+tf.set_random_seed(args.seed)
 sess = tf.Session()
 
 with tf.device('/cpu:0'):
-    global_net = ActorCriticNet(GLOBAL_NET_SCOPE, GAME)
-    workers = []
-    for i in range(N_WORKERS):
-        i_name = 'W_%i' % i
-        workers.append(Worker(i_name, GAME, global_net))
+    global_net = ActorCriticNet(args, 'global_net', sess)
+    average_net = ActorCriticNet(args, 'average_net', sess)
+    agents = []
+    for i in range(args.num_agents):
+        agent_name = 'Agent_%i' % i
+        agents.append(Agent(args, i, agent_name, global_net, average_net, sess))
+
+if args.output_graph:
+    tf.summary.FileWriter("logs/", sess.graph)
 
 coord = tf.train.Coordinator()
 sess.run(tf.global_variables_initializer())
+# initialize average network weights to global network weights
+sess.run([tf.assign(a_p, g_p) for a_p, g_p in zip(average_net.a_params, global_net.a_params)])
 
 
 def job():
-    worker.acer_main(sess, coord)
+    agent.acer_main(args, coord, average_net)
 
 
-worker_threads = []
-for worker in workers:
-    t = threading.Thread(target=job)
-    t.start()
-    worker_threads.append(t)
-coord.join(worker_threads)
-
-# for i in range(20):
-#     s = env.reset()
-#     while True:
-#         env.render()
-#         a = global_net.choose_action(s)
-#         s1, r, is_done, info = env.step(a)
-#         if is_done:
-#             break
-#         s = s1
-
-
-plt.plot(np.arange(len(ep_rewards)), ep_rewards)
-plt.xlabel('episode')
-plt.ylabel('reward')
-plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
+agent_threads = []
+for agent in agents:
+    task = threading.Thread(target=job)
+    task.start()
+    agent_threads.append(task)
+coord.join(agent_threads)
 
--- network.py
+++ network.py
@@ -9,151 +9,226 @@ import numpy as np
 import gym
 import math
 import random
-from replay_buffer import ReplayMemory
+from replay_memory import ReplayMemory
 
-
-N_HIDDEN = 200
-LEARNING_RATE_ACTOR = 0.001
-LEARNING_RATE_CRITIC = 0.001
-ENTROPY_BETA = 0.001
-GAMMA = 0.9
 global_iter = 0
+ep_num = 0
 ep_rewards = []
-MAX_ITERATION = 10000
-UPDATE_FREQ = 10
-MAX_STORED_EPISODES = 4000
-MAX_EPISODE_LENGTH = 1000
-REPLAY_RATIO = 4
 
 
 class ActorCriticNet(object):
-    def __init__(self, scope, game):
-        self.env = gym.make(game).unwrapped
+    def __init__(self, args, scope, sess=None):
+        self.sess = sess
+        self.env = gym.make(args.env).unwrapped
         self.input_size = self.env.observation_space.shape[0]
         self.output_size = self.env.action_space.n
         self.scope = scope
         self.s = tf.placeholder(tf.float32, [None, self.input_size], 'states')
         with tf.variable_scope(self.scope):
-            self.a_prob, self.v, self.a_params, self.c_params = self._build_net()
-            self.target_v = tf.placeholder(tf.float32, [None, 1], 'target_value')
+            self.a_prob, self.v, self.q, self.a_params, self.c_params = self._build_net(args)
+            self.target = tf.placeholder(tf.float32, [None, 1], 'target_value')
             self.a_his = tf.placeholder(tf.int32, [None, ], 'history_actions')
             self.rho = tf.placeholder(tf.float32, [None, 1], 'importance_sampling')
-            td_error = tf.subtract(self.target_v, self.v, name='td_error')
-            self.c_loss = tf.reduce_mean(tf.square(td_error))
-            neg_log_prob = tf.reduce_sum(-tf.log(self.a_prob) *
-                                         tf.one_hot(self.a_his, self.output_size, dtype=tf.float32),
-                                         axis=1,
-                                         keep_dims=True)
-            self.a_loss = tf.reduce_mean(neg_log_prob * td_error)
+            self.average_p = tf.placeholder(tf.float32, [None, self.output_size], 'average_policy')
 
-    def _build_net(self):
-        w_initializer = tf.random_normal_initializer(0., 0.1)
+    def _build_net(self, args):
         with tf.variable_scope('actor'):
-            l1 = tf.layers.dense(self.s, N_HIDDEN, tf.nn.relu,
-                                 kernel_initializer=w_initializer, name='l1')
-            a_prob = tf.layers.dense(l1, self.output_size, tf.nn.softmax,
-                                     kernel_initializer=w_initializer, name='l2_actor')
+            l1 = tf.layers.dense(self.s, args.num_hidden, tf.nn.relu, name='l1')
+            a_prob = tf.layers.dense(l1, self.output_size, tf.nn.softmax, name='l2_actor')
+
         with tf.variable_scope('critic'):
-            l1 = tf.layers.dense(self.s, N_HIDDEN, tf.nn.relu,
-                                 kernel_initializer=w_initializer, name='l1')
-            v = tf.layers.dense(l1, 1, kernel_initializer=w_initializer, name='l2_critic')
+            l1 = tf.layers.dense(self.s, args.num_hidden, tf.nn.relu, name='l1')
+            q = tf.layers.dense(l1, self.output_size, name='l2_q')
+            v = tf.reduce_sum(q * a_prob)  # V is expectation of Q under π
 
         a_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/actor')
         c_params = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope + '/critic')
-        return a_prob, v, a_params, c_params
+        return a_prob, v, q, a_params, c_params
 
-    def choose_action(self, s, sess):
+    #  choose action greedily
+    def choose_action(self, s, greedy=True):
         s = s[np.newaxis, :]
-        a_prob = sess.run(self.a_prob, {self.s: s})
-        return np.random.choice(np.arange(a_prob.shape[1]), p=a_prob.ravel()), np.arange(a_prob.shape[1])
+        a_prob = self.sess.run(self.a_prob, {self.s: s})
+        if greedy:
+            return np.argmax(a_prob.ravel())
+        else:
+            return np.random.choice(range(a_prob.shape[1]), p=a_prob.ravel())
+
+    def get_policy(self, s):
+        s = s[np.newaxis, :]
+        a_prob = self.sess.run(self.a_prob, {self.s: s})
+        return a_prob.ravel()
 
 
-class Worker(ActorCriticNet):
-    def __init__(self, scope, game, global_net: ActorCriticNet):
-        self.ep_num = 0
+class Agent(ActorCriticNet):
+    def __init__(self, args, rank, scope, global_net: ActorCriticNet, average_net: ActorCriticNet, sess=None):
+        super(Agent, self).__init__(args, scope, sess)
+        self.env.seed(args.seed + rank)
+        tf.set_random_seed(args.seed + rank)
+        self.memory = ReplayMemory(args.memory_capacity, args.max_stored_episode_length)
         self.name = scope
-        self.memory = ReplayMemory(MAX_STORED_EPISODES, MAX_EPISODE_LENGTH)
-        super(Worker, self).__init__(scope, game)
+        with tf.variable_scope(self.scope):
+            advantage = tf.subtract(self.target, self.v, name='advantage')
+            self.c_loss = 0.5 * tf.reduce_mean(tf.square(advantage))  # minimize advantage
+            # avoid NaN with clipping when value in policy becomes zero, or use
+            # neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=l2, labels=self.a_his)
+            neg_log_prob = tf.reduce_sum(-tf.log(tf.clip_by_value(self.a_prob, 1e-20, 1.0)) *
+                                         tf.one_hot(self.a_his, self.output_size, dtype=tf.float32),
+                                         axis=1,
+                                         keepdims=True)
+            # maximize total reward (log_p * R) is minimize -(log_p * R), tf can only minimize the loss
+            self.a_loss = self.rho * tf.reduce_mean(neg_log_prob * advantage)
+            self.kl_loss = -self._kl_div(self.average_p, self.a_prob)
+
         with tf.name_scope('local_gradients'):
             self.a_gradients = tf.gradients(self.a_loss, self.a_params)
+            kl_gradients = tf.gradients(self.kl_loss, self.a_params)
+            # Compute dot products of gradients
+            k_dot_g = sum(tf.reduce_sum(k_g * a_g) for k_g, a_g in zip(kl_gradients, self.a_gradients))
+            k_dot_k = sum(tf.reduce_sum(k_g ** 2) for k_g in kl_gradients)
+            # Compute trust region update
+            zero = tf.zeros_like(k_dot_k)
+            trust_factor = tf.where(tf.greater(k_dot_k, zero), tf.maximum((k_dot_g - args.delta) / k_dot_k, 0), zero)
+            self.total_gradients = [g_p - trust_factor * k_p for g_p, k_p in zip(self.a_gradients, kl_gradients)]
+            self.total_gradients = [tf.clip_by_norm(gradient, args.max_gradient_norm) for gradient in self.total_gradients]
             self.c_gradients = tf.gradients(self.c_loss, self.c_params)
+            self.c_gradients = [tf.clip_by_norm(gradient, args.max_gradient_norm) for gradient in self.c_gradients]
         with tf.name_scope('pull'):
             self.pull_a_params_op = [tf.assign(l_p, g_p) for l_p, g_p in zip(self.a_params, global_net.a_params)]
             self.pull_c_params_op = [tf.assign(l_p, g_p) for l_p, g_p in zip(self.c_params, global_net.c_params)]
         with tf.name_scope('update'):
-            self.train_op_a = tf.train.RMSPropOptimizer(LEARNING_RATE_ACTOR, name='RMSProp_Actor')
-            self.train_op_c = tf.train.RMSPropOptimizer(LEARNING_RATE_CRITIC, name='RMSProp_Critic')
-            self.update_a_op = self.train_op_a.apply_gradients(zip(self.a_gradients, global_net.a_params))
-            self.update_c_op = self.train_op_c.apply_gradients(zip(self.c_gradients, global_net.c_params))
+            self.optimizer_a = tf.train.RMSPropOptimizer(args.learning_rate, name='Optimizer_Actor')
+            self.optimizer_c = tf.train.RMSPropOptimizer(args.learning_rate, name='Optimizer_Critic')
+            self.update_a_op = self.optimizer_a.apply_gradients(zip(self.total_gradients, global_net.a_params))
+            self.update_c_op = self.optimizer_c.apply_gradients(zip(self.c_gradients, global_net.c_params))
+            # Update shared average policy network
+            self.update_average_net_op = [tf.assign(a_p, args.alpha * a_p + (1 - args.alpha) * g_p)
+                                          for a_p, g_p in zip(average_net.a_params, global_net.a_params)]
+    #         self.test1 = average_net.a_params
+    #         self.test2 = global_net.a_params
+    #
+    # def test(self):
+    #     print(self.sess.run(self.test1)[3])
+    #     print(self.sess.run(self.test2)[3])
 
-    def pull_global(self, sess):
-        sess.run([self.pull_a_params_op, self.pull_c_params_op])
+    def pull_global(self):
+        self.sess.run([self.pull_a_params_op, self.pull_c_params_op])
 
-    def update_global(self, sess, feed_dict):
-        sess.run([self.update_a_op, self.update_c_op], feed_dict)
+    def update_global(self, feed_dict):
+        self.sess.run([self.update_a_op, self.update_c_op], feed_dict)
 
-    def acer_main(self, sess, coord):
+    def update_average(self):
+        self.sess.run(self.update_average_net_op)
+
+    def acer_main(self, args, coord, average_net: ActorCriticNet):
         global global_iter
-        while not coord.should_stop() and global_iter < MAX_ITERATION:
-            self._train(sess, True)
-            n = self._get_poisson(REPLAY_RATIO)
-            for i in range(n):
-                self._train(sess, False)
+        while not coord.should_stop() and global_iter < args.max_iterations:
+            self._train(args, average_net, on_policy=True)
+            n = self._get_poisson(args.replay_ratio)
+            if len(self.memory) >= args.replay_start:
+                for i in range(n):
+                    self._train(args, average_net, on_policy=False)
 
-    def _train(self, sess, on_policy: bool):
+    def _train(self, args, average_net: ActorCriticNet, on_policy: bool):
+        global ep_num
+        t = 1
         # call on policy part, generate episode and save to replay memory
         if on_policy:
-            ep_r = 0
-            s = self.env.reset()
+            total_reward = 0
+            state = self.env.reset()
+            buffer_s, buffer_a, buffer_r, buffer_average_p, buffer_rho = [], [], [], [], []
             while True:
-                a, p = self.choose_action(s, sess)
-                next_s, r, is_done, info = self.env.step(a)
+                action = self.choose_action(state, greedy=False)
+                policy = self.get_policy(state)
+                next_state, reward, is_done, _ = self.env.step(action)
+                average_policy = average_net.get_policy(state)
+                buffer_average_p.append(average_policy)
+                total_reward += reward
+                buffer_s.append(state)
+                buffer_a.append(action)
+                buffer_r.append(reward)
+                buffer_rho.append(1.0)
+                self.memory.push(state, action, reward, policy, is_done)
+                if t % args.t_max == 0 or is_done:
+                    if is_done:
+                        v_target = 0
+                    else:
+                        v_target = self.sess.run(self.v, {self.s: next_state[np.newaxis, :]})
+                    buffer_v_target = []
+                    for r in buffer_r[::-1]:
+                        v_target = r + args.gamma * v_target
+                        buffer_v_target.append(v_target)
+                    buffer_v_target.reverse()
+                    buffer_s, buffer_a, buffer_v_target, buffer_rho = \
+                        np.vstack(buffer_s), np.array(buffer_a), np.vstack(buffer_v_target), np.vstack(buffer_rho)
+                    feed_dict = {
+                        self.s: buffer_s,
+                        self.a_his: buffer_a,
+                        self.target: buffer_v_target,
+                        self.rho: buffer_rho,
+                        self.average_p: buffer_average_p
+                    }
+                    # self.test()
+                    # print(self.sess.run(self.a_gradients, feed_dict)[3])
+                    self.update_global(feed_dict)
+                    buffer_s, buffer_a, buffer_r, buffer_average_p, buffer_rho = [], [], [], [], []
+                    self.pull_global()
+                    self.update_average()
+
+                # Increment counters
+                t += 1
+                state = next_state
                 if is_done:
-                    r = -5
-                ep_r += r
-                self.memory.push(s, a, r, p, is_done)
-                if is_done:
+                    ep_num += 1
+                    print('Thread:', self.name, 'Reward:', total_reward)
                     break
-                s = next_s
-            self.ep_num += 1
-            print('Thread:', self.name, 'Episode number:', self.ep_num, 'Reward:', ep_r)
+
         # call off policy part
         else:
-            samples = self.memory.sample(UPDATE_FREQ)
+            samples = self.memory.sample(args.batch_size)
             buffer_s = [m[0] for m in samples]
             buffer_a = [m[1] for m in samples]
             buffer_r = [m[2] for m in samples]
             buffer_p = [m[3] for m in samples]
             buffer_done = [m[4] for m in samples]
-
-            buffer_rho = []
-            for i in range(len(samples)):
-                pi = sess.run(self.a_prob, {self.s: buffer_s[i][np.newaxis, :]})[0, buffer_a[i]]
-                mu = buffer_p[i][buffer_a[i]]
-                buffer_rho.append(pi/mu)
-
+            # importance sampling
+            buffer_rho, buffer_average_p, buffer_q_ret = [], [], []
             if buffer_done[-1]:
                 q_ret = 0
             else:
-                q_ret = sess.run(self.v, {self.s: buffer_s[-1][np.newaxis, :]})[0, 0]
-            buffer_q_ret = []
-            for r_i in reversed(buffer_r):
-                q_ret = r_i + GAMMA * q_ret
-                buffer_q_ret.append(q_ret)
+                q_ret = self.sess.run(self.v, {self.s: buffer_s[-1][np.newaxis, :]})
+            for i in reversed(range(len(samples))):
+                pi = self.sess.run(self.a_prob, {self.s: buffer_s[i][np.newaxis, :]})[0][buffer_a[i]]
+                mu = buffer_p[i][buffer_a[i]]
+                rho_clipped = min(np.nan_to_num(pi / mu), args.c)
+                average_p = average_net.get_policy(buffer_s[i])
+                q_ret = buffer_r[i] + args.gamma * q_ret
+                [buffer.append(item) for buffer, item in zip((buffer_rho, buffer_average_p, buffer_q_ret),
+                                                             (rho_clipped, average_p, q_ret))]
+                # q_i = self.sess.run(self.q, {self.s: buffer_s[i][np.newaxis, :]})[0][buffer_a[i]]
+                # v_i = self.sess.run(self.v, {self.s: buffer_s[i][np.newaxis, :]})
+                # # q_ret = rho_clipped * (q_ret - q_i) + v_i
             buffer_q_ret.reverse()
+            buffer_rho.reverse()
+            buffer_average_p.reverse()
             buffer_s, buffer_a, buffer_q_ret, buffer_rho = \
                 np.vstack(buffer_s), np.array(buffer_a), np.vstack(buffer_q_ret), np.vstack(buffer_rho)
             feed_dict = {
                 self.s: buffer_s,
                 self.a_his: buffer_a,
-                self.target_v: buffer_q_ret,
-                self.rho: buffer_rho
+                self.target: buffer_q_ret,
+                self.rho: buffer_rho,
+                self.average_p: buffer_average_p
             }
-            self.update_global(sess, feed_dict)
-            self.pull_global(sess)
+            # self.test()
+            # print(self.sess.run(self.a_gradients, feed_dict)[3])
+            self.update_global(feed_dict)
+            self.pull_global()
+            self.update_average()
 
     # poisson random number by Knuth
-    def _get_poisson(self, r):
+    @staticmethod
+    def _get_poisson(r):
         l, k, p = math.exp(-r), 0, 1
         while True:
             k += 1
@@ -161,6 +236,10 @@ class Worker(ActorCriticNet):
             if not p > l:
                 break
         return k - 1
+
+    @staticmethod
+    def _kl_div(p, q):
+        return tf.reduce_sum(tf.multiply(p, tf.log(tf.clip_by_value(tf.div(p, q), 1e-20, 1.0))))
 
 
 
